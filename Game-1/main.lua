9c77f96bf7bc74b15ad5588bd9a914b2c48bf141
Dasun Nethsara <techsaralk.pro@gmail.com>
2025-01-18 21:27:41 +0530

    Implement basic Pac-Man movement and food interaction

--- Game-1/main.lua
+++ Game-1/main.lua
@@ -1,13 +1,29 @@
 _G.love = require("love")
 
 function love.load()
-    _G.number = 0
+    love.graphics.setBackgroundColor(0.8, 0.8, 0.8)
+    _G.pacman = {}
+    pacman.x = 200
+    pacman.y = 250
+    pacman.eat = false
+
+    _G.food_x = 600
 end
 
 function love.update(dt)
-    number = number + 1
+    pacman.x = pacman.x + 1
+
+    if pacman.x >= food_x + 20 then
+        pacman.eat = true
+    end
 end
 
 function love.draw()
-    love.graphics.print(number)
+    if not pacman.eat then
+        love.graphics.setColor(1, 1, 0)
+        love.graphics.rectangle("fill", food_x, 200, 70, 70)
+    end
+
+    love.graphics.setColor(1, 0, 1)
+    love.graphics.arc("fill", pacman.x, pacman.y, 70, 1, 5)
 end
